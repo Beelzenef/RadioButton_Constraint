cf166fbef1f38efe49f3e1058ce39fbd1e7d67a4
Elena G <elena.guzbla@gmail.com>
2017-10-25 13:10:11 +0200

    rdb clicks, visibility, DONE

--- app/src/main/java/com/example/radiobutton/MainActivity.java
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.radiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * Clase para mostrar layouts dependiendo de radioButtons en RadioGroups
+ * @author Elena G (Beelzenef)
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private View constraintParticular;
+    private View constraintCorporacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Identificando constraints
+        constraintParticular = findViewById(R.id.consL_DatosParticular);
+        constraintCorporacion = findViewById(R.id.consL_DatosCorporacion);
+    }
+
+    /**
+     * Metodo para gestion de clicks en radiobuttons
+     * @param v
+     */
+    public void onRadioBttonClicked(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.rdB_Corporativo:
+                viewParticular(false);
+                break;
+            case R.id.rdB_Particular:
+                viewParticular(true);
+                break;
+        }
+    }
+
+    /**
+     * Método para ocultar o mostrar los constraints integrados con include
+     * @param b
+     */
+    private void viewParticular (boolean b)
+    {
+        constraintParticular.setVisibility(b ? View.VISIBLE : View.GONE);
+        constraintCorporacion.setVisibility(b ? View.GONE : View.VISIBLE);
     }
 }
